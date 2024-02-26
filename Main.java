cdfe392ea06bdf79f76fa197fe7613e4b5b12dcb
Udunen <cole.huffine@gmail.com>
2024-02-26 10:02:02 -0600

    I LOVE COMMENTS

--- Main.java
+++ Main.java
@@ -13,8 +13,14 @@ public class Main {
         // this code is very effective until around 30 feet which is further than podium so it doesnt matter lol
         //      the reason its uneffective at until 30 is bc the note needs to follow the trajectory and enter the speaker
         //      before the trajectory reaches its apex and around 30 feet is when it reaches the apex
-        //      i mean if we increase velocity more around then it should work, not sure why it doesnt rn
-        //      but it doesnt matter
+        //      i mean if we increase velocity more around there it should work, not sure why it doesnt rn
+        //      but it doesnt matter because 30 feet is further than the middle of the field and were never 
+        //      gonna shoot from there because our robot isnt Steph Curry, and theres too many tiny variables
+        //      to shoot from that far (mfw our auto aim doesnt account for general wear and tear of each note)
+
+        // also if air resistance arises to be a problem, we can raise the magic variable until it works
+        //      which is the easy way but its not technically right. The correct way is to change a_x to
+        //      some accurate number (do research) then change some velocity variables to work with it
         
         // everything is in meters, radians, and seconds
 
@@ -24,16 +30,16 @@ public class Main {
         double arm_length = 24.727914/39.37;
         double shooter_length = 5.639686/39.37;
         
+        //-6.703, -5.60307862645667 is up against subwoofer
         double a_y = -9.8; // gravity
         double v_y = 0;
-        double p_y = -6.703/3.281; // vertical distance from target, this will change bc the arm will raise the further it gets away 6.703
-        double a_x = 0; // pretend this doesn't exist because I don't feel like doing air resistance
+        double p_y = -6.703/3.281; // vertical distance from target, this will change bc the arm will raise the further it gets away
+        double a_x = -0; // pretend this doesn't exist because I don't feel like doing air resistance
         double v_x = 0;
-        double p_x = -5.60307862645667/3.281; // horizontal distance from target, this will change bc the robot moves 5.60307862645667
+        double p_x = -5.60307862645667/3.281; // horizontal distance from target, this will change bc the robot moves
         
         // i swear this is the only magic number and its to align the arm to 0 when against the subwoofer
         // you could go through the math to get the good number but i dont feel like it so nah
-        // you can also think about it as accounting for air resistance
         double magic = 2.9998; // 2.9998
         double shootingvelo_y = Math.sqrt(p_y * a_y * 2) + magic;
         double shootingvelo_x = p_x/(shootingvelo_y/a_y) + magic;
@@ -42,7 +48,6 @@ public class Main {
             shootingvelo = 5700;
         }
         double rpm = (30*shootingvelo)/(Math.PI*(2.0/39.37));
-        // double shootingvelo = (2.0*Math.PI/60.0)*(2.0/39.37)*1280.0; // right
         
         double t4 = (Math.pow(a_x, 2) + Math.pow(a_y, 2))/4;
         double t3 = (a_x*v_x + a_y*v_y);
