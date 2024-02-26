64289ffa8491ea2fd87db25faad76f25f7003c61
Udunen <cole.huffine@gmail.com>
2024-02-25 22:52:14 -0600

    i think this is finally done

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
     RootFinder rootFinder = new RootFinder();
@@ -17,13 +20,17 @@ public class Main {
         
         double a_y = -9.8; // gravity
         double v_y = 0;
-        double p_y = -10; // vertical distance from target
+        double p_y = -6.703/3.281; // vertical distance from target, this will change bc the arm will raise the further it gets away
         double a_x = 0; // pretend this doesn't exist because I don't feel like doing air resistance
         double v_x = 0;
-        double p_x = -12; // horizontal distance from target
+        double p_x = -5.60307862645667/3.281; // horizontal distance from target, this will change bc the robot moves
         
-        double shootingvelo_y = Math.sqrt(p_y * a_y * 2);
-        double shootingvelo_x = p_x/(shootingvelo_y/a_y);
+        // i swear this is the only magic number and its to align the arm to 0 when against the subwoofer
+        // you could go through the math to get the good number but i dont feel like it so nah
+        // you can also think about it as accounting for air resistance
+        double magic = 2.9998; // 2.9998
+        double shootingvelo_y = Math.sqrt(p_y * a_y * 2) + magic;
+        double shootingvelo_x = p_x/(shootingvelo_y/a_y) + magic;
         double shootingvelo = Math.sqrt(Math.pow(shootingvelo_x, 2) + Math.pow(shootingvelo_y, 2));
         if(shootingvelo > 5700) {
             shootingvelo = 5700;
@@ -56,13 +63,14 @@ public class Main {
                 minRoot = root;
             }
         }
-
-
         if (minRoot == Double.MAX_VALUE) {
             System.out.println("no solutions");
             return;
         }
         t = minRoot;
+    
+
+
 
         // double maxRoot = Double.MIN_VALUE;
         // System.out.println(roots);
@@ -82,7 +90,7 @@ public class Main {
         double p_aimX = -(p_x + v_x*t + (a_x*(Math.pow(t, 2)))/2.0);
         double p_aimY = -(p_y + v_y*t + (a_y*(Math.pow(t, 2)))/2.0);
         double shooting_theta = Math.atan(p_aimY / p_aimX);
-        double arm_theta = shooting_theta + (35.0*Math.PI/180.0);
+        double arm_theta = shooting_theta - (55*Math.PI/180.0);
         // System.out.println(t);
 
         try {
@@ -90,12 +98,14 @@ public class Main {
             desmos.write("(" + p_aimX + ", " + p_aimY + ")\n");
             desmos.write("(" + -p_x + ", " + -p_y + ")\n");
             desmos.write("f\\left(x\\right)=" + p_aimY / p_aimX + "x-" + "\\frac{" + (-a_y) + "x^{2}}{2\\cdot\\left(" + shootingvelo + "\\right)^{2}\\cdot\\cos^{2}\\left(" + shooting_theta + "\\right)}\n");
-            desmos.write("a_{degrees}=" + (arm_theta*(180.0/Math.PI)) + "\n");
+            desmos.write("a_{degrees}=" + (-arm_theta*(180.0/Math.PI)) + "\n");
             desmos.write("\\operatorname{polygon}((0,0),(" + -shooter_length * Math.cos(shooting_theta) + "," + -shooter_length * Math.sin(shooting_theta) + "))\n");
-            desmos.write("\\operatorname{polygon}((" + -shooter_length * Math.cos(shooting_theta) + "," + -shooter_length * Math.sin(shooting_theta) + "),(" + arm_length * Math.cos(arm_theta) + "," + -arm_length * Math.sin(arm_theta) + "))\n");
+            desmos.write("\\operatorname{polygon}((" + -shooter_length * Math.cos(shooting_theta) + "," + -shooter_length * Math.sin(shooting_theta) + "),(" + (-shooter_length * Math.cos(shooting_theta) + arm_length * Math.cos(arm_theta)) + "," + (-shooter_length * Math.sin(shooting_theta) + arm_length * Math.sin(arm_theta)) + "))\n");
             desmos.write("r_{pm}=" + rpm);
             desmos.close();
-        } catch (IOException e) {
+
+            Desktop.getDesktop().browse(new URI("https://www.desmos.com/calculator"));
+        } catch (IOException | URISyntaxException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
